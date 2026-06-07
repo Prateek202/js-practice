6c6b92ebe9d1c0393cfd75e45c8dd5ec7dcebd40
Prateek Thakur <prateek.2022ca069@mnnit.ac.in>
2026-06-07 19:48:26 +0530

    operations

---
+++ basics/7_basics.js
@@ -0,0 +1,22 @@
+//******************Operations****************************
+
+let val = 3
+let negVal = -val
+//console.log(negVal)
+
+let s1 = "Prateek"
+let s2 = "Hello"
+// console.log(s2 + s1)
+
+
+//if the string is at start like 3rd then consider all as string if string is at end so perform operation then add string 
+// console.log("1" + 2)    //12        
+// console.log(1 + "2")    //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + "2"  + 2) //122  
+// console.log(1 + 2  + "2") //32  
+
+
+console.log(+true)
+console.log(+"")
+
