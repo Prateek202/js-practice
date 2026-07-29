5713a9ba2aad048787e9bbfe42ef29eed68e9561
Prateek Thakur <prateek.2022ca069@mnnit.ac.in>
2026-07-29 12:34:04 +0530

    Scopes let var const

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+// let a = 5
+// var b = 2
+// const c = 4
+let a = 12
+
+if(true){
+    let a = 5
+    var b = 2
+    const c = 4
+    console.log("inner: ", a)
+}
+console.log(a)
+// console.log(b)
+// console.log(c)
